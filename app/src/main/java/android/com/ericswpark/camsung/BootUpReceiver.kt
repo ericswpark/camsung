7bb3c5865d5f2e0024a36d49931c5633da655b32
Eric Park <me@ericswpark.com>
2023-03-12 15:08:34 +0900

    fix: make BroadcastReceiver safe from 3rd-party calls
    ACTION_BOOT_COMPLETED can only be sent by the system; however, we are
    not checking for that specific intent in our BroadcastReceiver class.
    This allows any third-party app to call our BR class and execute the
    code, which is not what we want. To fix this, check the intent received
    before executing any code.
    
    Further information here: https://stackoverflow.com/a/52628842

--- app/src/main/java/android/com/ericswpark/camsung/BootUpReceiver.kt
+++ app/src/main/java/android/com/ericswpark/camsung/BootUpReceiver.kt
@@ -13,17 +13,21 @@ class BootUpReceiver : BroadcastReceiver() {
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context, intent: Intent?) {
-        val sharedPref = context.getSharedPreferences("android.com.ericswpark.camsung.PREFERENCES",
-            Context.MODE_PRIVATE)
-        val startBootEnabled = (sharedPref.getInt("start_at_boot", 0) == 1)
+        when (intent!!.action) {
+            Intent.ACTION_BOOT_COMPLETED -> {
+                val sharedPref = context.getSharedPreferences("android.com.ericswpark.camsung.PREFERENCES",
+                    Context.MODE_PRIVATE)
+                val startBootEnabled = (sharedPref.getInt("start_at_boot", 0) == 1)
 
-        if (startBootEnabled) {
-            if (Settings.System.canWrite(context)) {
-                CameraHelper.setCameraMute(context.contentResolver)
-                Toast.makeText(context, R.string.main_activity_mute_enabled, Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(context, R.string.boot_error_no_permissions, Toast.LENGTH_SHORT)
-                    .show()
+                if (startBootEnabled) {
+                    if (Settings.System.canWrite(context)) {
+                        CameraHelper.setCameraMute(context.contentResolver)
+                        Toast.makeText(context, R.string.main_activity_mute_enabled, Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, R.string.boot_error_no_permissions, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
             }
         }
     }
