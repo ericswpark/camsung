cbb88e77840a0294964b93268ba9e94988c377fa
Eric Park <me@ericswpark.com>
2025-04-28 13:09:29 -0400

    refactor: disable spellcheck and use constant naming

--- app/src/main/java/android/com/ericswpark/camsung/CameraHelper.kt
+++ app/src/main/java/android/com/ericswpark/camsung/CameraHelper.kt
@@ -5,11 +5,12 @@ import android.provider.Settings
 
 class CameraHelper {
     companion object {
-        private const val propString = "csc_pref_camera_forced_shuttersound_key"
+        @Suppress("SpellCheckingInspection")
+        private const val PROP_STRING = "csc_pref_camera_forced_shuttersound_key"
 
         fun isCameraMuted(contentResolver: ContentResolver): Boolean {
             return try {
-                val propValue = Settings.System.getInt(contentResolver, propString)
+                val propValue = Settings.System.getInt(contentResolver, PROP_STRING)
                 propValue == 0
             } catch (e: Settings.SettingNotFoundException) {
                 false
@@ -17,11 +18,11 @@ class CameraHelper {
         }
 
         fun setCameraMute(contentResolver: ContentResolver) {
-            Settings.System.putInt(contentResolver, propString, 0)
+            Settings.System.putInt(contentResolver, PROP_STRING, 0)
         }
 
         fun setCameraUnmute(contentResolver: ContentResolver) {
-            Settings.System.putInt(contentResolver, propString, 1)
+            Settings.System.putInt(contentResolver, PROP_STRING, 1)
         }
     }
 }
