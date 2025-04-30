23f9cc4568da594325e460d3e7f9bea1460b9cf9
Eric Park <me@ericswpark.com>
2025-04-29 20:45:10 -0400

    build: attempt at automated screenshots
    Unfortunately this does not yet work because fastlane is unaware of the
    override required for installing an APK targeting a deprecated SDK...

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source "https://rubygems.org"
 gem "fastlane", "~> 2.227"
+
+gem "screengrab", "~> 1.0"
--- app/src/androidTest/java/android/com/ericswpark/camsung/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/android/com/ericswpark/camsung/ExampleInstrumentedTest.kt
@@ -1,12 +1,12 @@
 package android.com.ericswpark.camsung
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.ClassRule
 import org.junit.Test
 import org.junit.runner.RunWith
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.locale.LocaleTestRule
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +15,18 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @ClassRule
+    val localeTestRule: LocaleTestRule = LocaleTestRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("android.com.ericswpark.camsung", appContext.packageName)
+    fun testTakeScreenshot() {
+        Screengrab.screenshot("main")
     }
+
+//    @Test
+//    fun useAppContext() {
+//        // Context of the app under test.
+//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+//        assertEquals("android.com.ericswpark.camsung", appContext.packageName)
+//    }
 }
