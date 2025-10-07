7ed6a9281688d4e61788a216bbed84ab6009aff9
Eric Park <me@ericswpark.com>
2025-10-07 11:09:47 -0400

    refactor(FAQActivity): clean up arraylist init

--- app/src/main/java/android/com/ericswpark/camsung/faq/FAQActivity.kt
+++ app/src/main/java/android/com/ericswpark/camsung/faq/FAQActivity.kt
@@ -6,24 +6,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class FAQActivity : AppCompatActivity() {
+    val faqList = arrayListOf(
+        FAQ(getString(R.string.faq_activity_phone_mute_title),
+            getString(R.string.faq_activity_phone_mute_description)),
+        FAQ(getString(R.string.faq_activity_samsung_update_title),
+            getString(R.string.faq_activity_samsung_update_description)),
+        FAQ(getString(R.string.faq_activity_android_warning_title),
+            getString(R.string.faq_activity_android_warning_description))
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faq)
         setTitle(R.string.main_activity_action_faq)
 
         val recyclerView = findViewById<RecyclerView>(R.id.faq_recycler_view)
-
-        val faqList = ArrayList<FAQ>()
-
-        // Define all FAQs here
-        faqList.add(FAQ(getString(R.string.faq_activity_phone_mute_title),
-                getString(R.string.faq_activity_phone_mute_description)))
-        faqList.add(FAQ(getString(R.string.faq_activity_samsung_update_title),
-                getString(R.string.faq_activity_samsung_update_description)))
-        faqList.add(FAQ(getString(R.string.faq_activity_android_warning_title),
-            getString(R.string.faq_activity_android_warning_description)))
         val faqAdapter = FAQAdapter(faqList)
-
         recyclerView.adapter = faqAdapter
     }
 }
